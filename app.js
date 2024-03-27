4e7f48d3bedf7f0c2ef7a364e738e54bad09d1e5
Robotics <robotics@Cloud-Mini-Robotics.local>
2024-03-26 19:23:16 -0600

    Major Rework

--- app.js
+++ app.js
@@ -6,24 +6,145 @@ const greenLight = new Gpio(21, "out")
 const express = require('express')
 
 const app = express()
+clearAll()
+
+var greenLightLength = 20000
+var redLightLength = 20000
+var yellowLightLength = 4000
+
+var redLightBlinkLength = 4000
+var yellowLightBlinkLength = 4000
+
+var isRunningNormal = false
+var isRunningBlink = false
+
+app.get('/timeconfig/:color/:length/:type', (request, response) => {
+     if(request.params.type == "normal") {
+        if(request.params.color.toLowerCase() == "red") {
+            redLightLength = parseInt(request.params.length)
+            response.sendStatus(200)
+            return
+         }
+         if(request.params.color.toLowerCase() == "yellow") {
+            yellowLightLength = parseInt(request.params.length)
+            response.sendStatus(200)
+            return 
+         }
+         if(request.params.color.toLowerCase() == "green") {
+            greenLightLength = parseInt(request.params.length)
+            response.sendStatus(200)
+            return
+         }
+     }
+     if(request.params.type == "blink") {
+        if(request.params.color.toLowerCase() == "red") {
+            redLightBlinkLength = parseInt(request.params.length)
+            response.sendStatus(200)
+            return
+         }
+         if(request.params.color.toLowerCase() == "yellow") {
+            yellowLightLength = parseInt(request.params.length)
+            yellowLightBlinkLength.sendStatus(200)
+            return
+         }
+     }
+     response.sendStatus(400)
+    return
+})
+//color - red, yellow, green, none
+app.get('/setLight/:color', (request, response)=> {
+    isRunningNormal = false
+    isRunningBlink = false // clearAll()
+    if(request.params.color == "none") {
+
+    } else if(request.params.color == "green") {
+        setGreenOnly()
+        response.sendStatus(200)
+        return
+    } else if(request.params.color == "yellow") {
+        setYellowOnly()
+        response.sendStatus(200)
+        return 
+    } else if(request.params.color == "red") {
+        setRedOnly()
+        response.sendStatus(200)
+        return
+    }
+    response.sendStatus(400)
+    return
+})
+//Types = Normal, BlinkYellow, BlinkRed
+app.get('/setFunction/:type', (request, response)=> {
+    if(request.params.type == "normal") {
+        isRunningNormal = false
+        isRunningBlink = false
+        setNormalLight()
+        response.sendStatus(200)
+        return
+    } else if(request.params.type == "blinkyellow") {
+        isRunningNormal = false
+        isRunningBlink = false
+        setBlink("yellow")
+        response.sendStatus(200)
+        return
+    } else if(request.params.type == "blinkred") {
+        isRunningNormal = false
+        isRunningBlink = false
+        setBlink("red")
+        response.sendStatus(200)
+        return
+    }
+})
+
+function setBlink(color) {
+    if(!isRunningBlink) {return}
+    if(color == "yellow") {
+        if(!isRunningBlink) {return}
+        setYellowOnly()
+        setTimeout(()=> {
+            if(!isRunningBlink) {return}
+            clearAll()
+            setTimeout(()=> {
+                if(!isRunningBlink) {return}
+                setBlink(color)
+            }, yellowLightBlinkLength)
+        }, yellowLightBlinkLength)
+    } else  if(color == "red") {
+        if(!isRunningBlink) {return}
+        setRedOnly()
+        setTimeout(()=> {
+            if(!isRunningBlink) {return}
+            clearAll()
+            setTimeout(()=> {
+                if(!isRunningBlink) {return}
+                setBlink(color)
+            }, yellowLightBlinkLength)
+        }, yellowLightBlinkLength)
+    }
+}
+
 
 function setNormalLight() {
     //First go to green
     // var lightInterval = setInterval(()=> {
+        if(!isRunningNormal) {return}
+        isRunningNormal = true
         setGreenOnly()
         setTimeout(()=> {
+            if(!isRunningNormal) {return}
             setYellowOnly()
             setTimeout(()=> {
                 setRedOnly()
-
+                if(!isRunningNormal) {return}
                 setTimeout(()=> {
+                    if(!isRunningNormal) {return}
                     setNormalLight()
-                }, 20000)
-            }, 4000)
-        }, 20000)   
-
-
+                }, redLightLength)
+            }, yellowLightLength)
+        }, greenLightLength)   
 }
+
+
 
 setNormalLight()
 function setRedOnly() {
@@ -42,6 +163,12 @@ function setGreenOnly() {
     redLight.write(0)
     yellowLight.write(0)
     greenLight.write(1)
+}
+
+function clearAll() {
+    redLight.write(0)
+    yellowLight.write(0)
+    greenLight.write(0)
 }
 
 
