508771a7ce08476803af2cd79e9d070d7afe8ae3
Robotics <robotics@Cloud-Mini-Robotics.local>
2024-03-26 18:42:25 -0600

    Added Normal Light, express

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 const Gpio = require('onoff').Gpio
-const led = new Gpio(16, "out")
+const redLight = new Gpio(16, "out")
+const yellowLight = new Gpio(20, "out")
+const greenLight = new Gpio(21, "out")
 
-led.write(0)
+const express = require('express')
+
+const app = express()
+
+function setNormalLight() {
+    //First go to green
+    var lightInterval = setInterval(()=> {
+        setGreenOnly()
+        setTimeout(()=> {
+            setYellowOnly()
+            setTimeout(()=> {
+                setRedOnly()
+            }, 2000)
+        }, 2000)   
+    })
+
+}
+
+setNormalLight()
+function setRedOnly() {
+    redLight.write(1)
+    yellowLight.write(0)
+    greenLight.write(0)
+}
+
+function setYellowOnly() {
+    redLight.write(1)
+    yellowLight.write(0)
+    greenLight.write(0)
+}
+
+function setGreenOnly() {
+    redLight.write(1)
+    yellowLight.write(0)
+    greenLight.write(0)
+}
+
+
+app.listen(7890, () => {
+    console.log(`Server is Listening on 7890`)
+})
